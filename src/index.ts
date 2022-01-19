8aa026180415e251018b89ef01290627a4b10e72
andreylazeev <lazeev1998@yandex.ru>
2022-01-20 03:07:01 +0700

    procfile added

--- src/index.ts
+++ src/index.ts
@@ -15,25 +15,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('hello')
 })
 
-const throttle = (fn: Function, wait: number = 300) => {
-  let inThrottle: boolean, lastFn: ReturnType<typeof setTimeout>, lastTime: number
-  return function (this: any) {
-    const context = this,
-      args = arguments
-    if (!inThrottle) {
-      fn.apply(context, args)
-      lastTime = Date.now()
-      inThrottle = true
-    } else {
-      clearTimeout(lastFn)
-      lastFn = setTimeout(() => {
-        if (Date.now() - lastTime >= wait) {
-          fn.apply(context, args)
-          lastTime = Date.now()
-        }
-      }, Math.max(wait - (Date.now() - lastTime), 0))
-    }
-  }
-}
-
 server.listen(5000, () => console.log('server started'))
